fc819168da967115a23850d064bbd3c2f4fe4cb3
Narendra kumar <148026989+Narendra4405@users.noreply.github.com>
2025-09-01 15:17:35 +0530

    Add files via upload
    and the second one are app files

--- urls.py
+++ urls.py
@@ -1,27 +1,37 @@
-"""
-URL configuration for docmanager project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
-from django.urls import path, include
-from django.shortcuts import redirect
+from . import views
+
+app_name = 'db_manager'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('db-manager/', include('db_manager.urls')),
-    path('', lambda request: redirect('db_manager:dashboard')),
+    # Dashboard
+    path('', views.dashboard, name='dashboard'),
+     path("test-task/", views.test_task, name="test_task"),
+     path('logs/<int:log_id>/delete/', views.delete_query_log, name='delete_query_log'),
+
+    
+    # Database Connections
+    path('connections/', views.connection_list, name='connection_list'),
+    path('connections/add/', views.add_connection, name='add_connection'),
+    path('connections/<int:connection_id>/edit/', views.edit_connection, name='edit_connection'),
+    path('connections/<int:connection_id>/test/', views.test_connection_view, name='test_connection'), #for test button
+    
+    # Query Interface
+    path('connections/<int:connection_id>/query/', views.query_interface, name='query_interface'),
+    
+    # Monitoring
+    path('connections/<int:connection_id>/monitoring/', views.monitoring_config, name='monitoring_config'),
+    path('connections/<int:connection_id>/stats/', views.database_stats_view, name='database_stats'),
+    
+    # Logs
+    path('logs/', views.query_logs, name='query_logs'),
+    path('connections/<int:connection_id>/logs/', views.query_logs, name='connection_logs'),
+    
+    # API Endpoints
+    path('api/execute-query/', views.api_execute_query, name='api_execute_query'),
+    path('api/connections/<int:connection_id>/stats/', views.api_connection_stats, name='api_connection_stats'),
+
+
 ]
+
 
